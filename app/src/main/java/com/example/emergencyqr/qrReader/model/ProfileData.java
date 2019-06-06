633f01309d6dfec2c5617da149a6fe38de44e956
theminja <retrominh@gmail.com>
2019-06-05 18:47:07 -0700

    working on login, qrreader, and profile screens

---
+++ app/src/main/java/com/example/emergencyqr/qrReader/model/ProfileData.java
@@ -0,0 +1,51 @@
+package com.example.emergencyqr.qrReader.model;
+
+public class ProfileData {
+
+    private String name;
+    private int age;
+    private String address;
+    //TODO datatype for location
+    private String emergencyContactNumber;
+    private String medicalProvider;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmergencyContactNumber() {
+        return emergencyContactNumber;
+    }
+
+    public void setEmergencyContactNumber(String emergencyContactNumber) {
+        this.emergencyContactNumber = emergencyContactNumber;
+    }
+
+    public String getMedicalProvider() {
+        return medicalProvider;
+    }
+
+    public void setMedicalProvider(String medicalProvider) {
+        this.medicalProvider = medicalProvider;
+    }
+}
