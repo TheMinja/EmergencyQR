bda62f3c40851bc8d55645bcaff3b20654e4c5cb
theminja <retrominh@gmail.com>
2019-06-16 15:10:04 -0700

    Code cleanup

--- app/src/main/java/com/retro/emergencyqr/activities/LoginActivity.java
+++ app/src/main/java/com/retro/emergencyqr/activities/LoginActivity.java
@@ -1,8 +1,7 @@
-package com.retro.emergencyqr.activities.login;
+package com.retro.emergencyqr.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
@@ -13,7 +12,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.retro.emergencyqr.R;
-import com.retro.emergencyqr.activities.BaseActivity;
 import com.retro.emergencyqr.activities.qrReader.QRScanActivity;
 import com.retro.emergencyqr.framework.presenter.LoginPresenter;
 import com.retro.emergencyqr.framework.view.LoginView;
